3af1a5adc780f4baffd51520e72450979a00aa74
Jesus Miguel Marcelino Kranwinkel <jesus.marcelinok@gmail.com>
2020-02-19 19:48:26 -0400

    Ejercicio 2

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        //Crear un metodo que recorra los numeros del 1-50 usando un bucle for. 
+        //Llamar el metodo desde el Main.
+        static void Main(string[] args)
+        {
+            Recorrer(1, 50);
+            Console.Read();
+        }
+        static void Recorrer(int desde, int hasta)
+        {
+            for(int i = desde; i <= hasta; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
