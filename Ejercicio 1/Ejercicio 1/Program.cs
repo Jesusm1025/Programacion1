11960b90de8a5e3edcf3f36ed8557ff5a5eb1511
Jesus Miguel Marcelino Kranwinkel <jesus.marcelinok@gmail.com>
2020-02-19 19:36:43 -0400

    Ejercicio 1

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        /*Crear un metodo llamado "HolaEstudiante" con el valor predefinido (Su nombre) y
+         llamarlo desde el Main muestre el saludo ("Hola, estudiante XX. 
+         Bienvenido a programación I").*/
+        static void Main(string[] args)
+        {
+            Console.Write(HolaEstudiante("Jesus"));
+            Console.Read();
+        }
+        static string HolaEstudiante(string nombre)
+        {
+            string HolaEstudiante = "Hola, estudiantes " + nombre + ". Bienvenido a programacion I";
+            return HolaEstudiante;
+        }
+    }
+}
